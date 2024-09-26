7684098f5e942dd8d126bdcacc0bfbb691d22490
kt194 <kt194@example.com>
2024-09-26 10:54:18 +0000

    非ログイントップページ作成

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ログイン機能付き買い物リスト管理サービス</title>
+    </head>
+    <body>
+        <h1>ログイン</h1>
+        <form action="/login" method="post">
+            email:<input type="email" name="email"/><br>
+            パスワード:<input type="password" name="password"/><br>
+            <button>ログインする</button><br>
+            <a href="#">会員登録</a>
+        </form>
+    </body>
+</html>
